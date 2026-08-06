84a5d7bc6376a4708bddeb66d9aeca25ac7a1161
kashishkashish2023 <kashishkashish2305@gmail.com>
2026-08-06 22:53:07 +0530

    Fix Gmail transporter

--- backend/server.js
+++ backend/server.js
@@ -510,18 +510,11 @@ app.get("/place/:id", async(req,res)=>{
 
 
 const transporter = nodemailer.createTransport({
-
-    host: "smtp.gmail.com",
-
-    port: 587,
-
-    secure: false,
-
-    auth:{
+    service: "gmail",
+    auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     }
-
 });
 
 
