34cbd39515cc270c98920f6d5b92189dfa2c30f5
kashishkashish2023 <kashishkashish2305@gmail.com>
2026-08-07 02:34:40 +0530

    Fix Gmail SMTP connection

--- backend/server.js
+++ backend/server.js
@@ -510,13 +510,14 @@ app.get("/place/:id", async(req,res)=>{
 
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     }
 });
-
 
 
 app.post("/api/contact", async(req,res)=>{
